ec1733f0232630126b367178f0bfcd7c9a708c7e
Anand Choudhary <anandpc13@gmail.com>
2018-06-19 06:03:40 +0530

    Calc UI Completed

--- app/src/main/java/com/techieandy/anand/calc/MainActivity.java
+++ app/src/main/java/com/techieandy/anand/calc/MainActivity.java
@@ -2,6 +2,10 @@ package com.techieandy.anand.calc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,3 +15,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
