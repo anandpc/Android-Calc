33fce86d867551c6b0421006a592a60557909265
Anand Choudhary <anandpc13@gmail.com>
2018-06-21 20:52:23 +0530

    Calc UI Completed

--- app/src/main/java/com/techieandy/anand/calc/MainActivity.java
+++ app/src/main/java/com/techieandy/anand/calc/MainActivity.java
@@ -9,10 +9,22 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView input = findViewById(R.id.input),result = findViewById(R.id.ans);
+    Button AC,C;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void all_clear(View view) {
+            input.setText("0");
+            result.setText("0");
+    }
+
+    public void clear(View view) {
+            input.setText("0");
     }
 }
 
